bfd56dc11ddcd5e41b5a7475ebd25288f720338a
Akhilasivakumar <114943894+Akhilasivakumar@users.noreply.github.com>
2026-03-03 16:37:13 +0530

    second commit

--- k6-monitoring-stack/test.js
+++ k6-monitoring-stack/test.js
@@ -7,12 +7,12 @@ export const options = {
 };
 
 export default function () {
-    // Simulating different pages mentioned in the report
-    http.get('https://jsonplaceholder.typicode.com/posts', { tags: { name: 'Home page' } });
-    http.get('https://jsonplaceholder.typicode.com/this-page-does-not-exist', { tags: { name: 'Broken Page' } });
-    http.get('https://jsonplaceholder.typicode.com/posts/1', { tags: { name: 'Articles' } });
-    http.get('https://jsonplaceholder.typicode.com/comments', { tags: { name: 'Draw & Pools' } });
-    http.get('https://jsonplaceholder.typicode.com/users', { tags: { name: 'Registration page' } });
+    // Testing different API endpoints
+    http.get('https://jsonplaceholder.typicode.com/posts', { tags: { name: 'GET /posts (All Posts)' } });
+    http.get('https://jsonplaceholder.typicode.com/this-page-does-not-exist', { tags: { name: 'GET /invalid (404 Test)' } });
+    http.get('https://jsonplaceholder.typicode.com/posts/1', { tags: { name: 'GET /posts/1 (Single Post)' } });
+    http.get('https://jsonplaceholder.typicode.com/comments', { tags: { name: 'GET /comments (All Comments)' } });
+    http.get('https://jsonplaceholder.typicode.com/users', { tags: { name: 'GET /users (All Users)' } });
 
     sleep(1);
 }
